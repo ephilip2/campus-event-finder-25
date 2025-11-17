f15519c32152fdbf3bb63d7e1e3749ac2b9fce10
elisa <ephilip2@illinois.edu>
2025-11-16 23:26:41 -0600

    Implement /event route for retrieving full event details
      Add server endpoint and client method to fetch complete event data by ID. Server uses
      efficient map-based lookups instead of linear search. Includes comprehensive test coverage
       validating all event fields.

---
+++ app/src/main/kotlin/edu/illinois/cs/cs124/ay2025/mp/activities/EventActivity.kt
@@ -0,0 +1,10 @@
+package edu.illinois.cs.cs124.ay2025.mp.activities
+
+import android.app.Activity
+import android.os.Bundle
+
+class EventActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/kotlin/edu/illinois/cs/cs124/ay2025/mp/models/Event.kt
@@ -0,0 +1,3 @@
+package edu.illinois.cs.cs124.ay2025.mp.models
+
+typealias Event = EventData
